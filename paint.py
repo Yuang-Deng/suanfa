bb8cfb55e9063684d909b4382b63abc480667f9e
hittrickcr7 <lnsydengyuang@163.com>
2020-10-17 10:32:48 +0800

    paint

--- paint.py
+++ paint.py
@@ -1,18 +1,30 @@
+import networkx as nx
 import matplotlib.pyplot as plt
 
-#分别存放所有点的横坐标和纵坐标，一一对应
-x_list = []
-y_list = []
+G = nx.DiGraph()
 
-#创建图并命名
-plt.figure('Line fig')
-ax = plt.gca()
-#设置x轴、y轴名称
-ax.set_xlabel('x')
-ax.set_ylabel('y')
+edgelist = []
+nodelist = {}
 
-#画连线图，以x_list中的值为横坐标，以y_list中的值为纵坐标
-#参数c指定连线的颜色，linewidth指定连线宽度，alpha指定连线的透明度
-ax.plot(x_list, y_list, color='r', linewidth=1, alpha=0.6)
+fe=open("SCC.in","r")
+edge=fe.read().splitlines()[1:]
+for line in edge:
+    edgelist.append((line.split(' ')[0],line.split(' ')[1]))
+G.add_edges_from(edgelist)
 
+fn=open("ConnectedComponents.out","r")
+nodes=fn.read().splitlines()
+for line in nodes:
+    nodelist[line.split('-')[0]]=int(line.split('-')[1])/10
+
+values = [nodelist.get(node) for node in G.nodes()]
+
+# Need to create a layout when doing
+# separate calls to draw nodes and edges
+# pos = nx.spring_layout(G)
+# nx.draw_networkx_nodes(G, pos, cmap=plt.get_cmap('jet'),
+#                        node_color = values, node_size = 500)
+# nx.draw_networkx_labels(G, pos)
+# nx.draw_networkx_edges(G, pos=nx.random_layout(G), edgelist=edgelist, arrows=True,width=0.3,style='solid',font_size=8)
+nx.draw_networkx(G,node_size=100, font_size=5, pos=nx.random_layout(G), edgelist=edgelist, arrows=True, width=0.3, style='solid', node_color=values)
 plt.show()
